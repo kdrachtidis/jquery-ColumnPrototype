cc7595811024d10567ebf718b4da1326673763b0
Konstantinos Drachtidis <konstantinos.drachtidis@sap.com>
2020-02-17 12:58:21 +0100

    Left sidebar JSON opti

--- sources/script/importLeftSidebar.js
+++ sources/script/importLeftSidebar.js
@@ -2,23 +2,29 @@ function leftSidebarJSON(data) {
     //Panel Head
     $("#drop-groupslist .label, #leftSidebar .fd-panel__title").append(data.title);
     $("#drop-groupslist .label, #leftSidebar .fd-panel__description").append(data.description);
-    $("#drop-groupslist .count, #leftSidebar .fd-panel__footer").append(data.items.length + " items loaded succesfully.");
 
     //Panel Body - list
-    $(data.items).each(function () {
+    $.each(data.regions, function (i, item) {
         var htmlItem;
 
-        htmlItem = '<li class="fd-list__item">';
-        htmlItem += '   <span class="fd-list__title">' + this.header + '</span>';
-        htmlItem += '</li>';
-        $(htmlItem).appendTo("#leftSidebar .fd-list");
+        htmlItem = '<li class="fd-list__group-header" id="group-' + this.id + '">' + this.name + ' ' + this.id + ' (' + this.items.length + ' items)</li>';
+        $(htmlItem).appendTo('#leftSidebar .fd-list');
 
-        if (this.header.length < 17) {
+        $(data.regions[i].items).each(function () {
+            var htmlItem;
+    
+            htmlItem = '<li class="fd-list__item">';
+            htmlItem += '   <span class="fd-list__title">' + this.header + ' ' + this.id + '</span>';
+            htmlItem += '</li>';
+            $(htmlItem).insertAfter('#leftSidebar #group-' + data.regions[i].id);
+    
             $('<option>' + this.header + '</option>').appendTo("#drop-groupslist select");
-        } else {
-            $('<option>' + this.header.substr(0, 16) + '...</option>').appendTo("#drop-groupslist select");
-        }
+            
+        });
     });
+
+    //Panel Footer
+    $("#drop-groupslist .count, #leftSidebar .fd-panel__footer").append(data.regions.length + ' groups of tenants successfully loaded.');
 }
 
 function getLeftSidebarData(customPath) {
